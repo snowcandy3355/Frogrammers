29560cdd339d3feb5f6b2489a0dc7b11d10308b4
hamzzi17 <Hammzzzi17@gmail.com>
2025-04-24 17:28:18 +0900

    인게임 ESC 메뉴 생성(Options 아직 작동 X)

---
+++ Assets/2. Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f; // 게임 일시정지
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f; // 게임 재개
+        }
+    }
+
+    public void ResumeGame()
+    {
+        TogglePause();
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("옵션창 열기 (추후 구현)");
+    }
+}
